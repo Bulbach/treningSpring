80da9db91ea63228d22b6c076bf616de85515f67
Bulbash <zverovik@yahoo.com>
2022-05-22 12:33:49 +0300

    application is started. Controller work

---
+++ src/main/java/com/alex/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.alex.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.alex")
+public class WebConfig implements WebMvcConfigurer {
+
+}
---
+++ src/main/java/com/alex/controllers/HumanController.java
@@ -0,0 +1,16 @@
+package com.alex.controllers;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/humans")
+public class HumanController {
+    @GetMapping("/check")
+    public String check(ModelMap model) {
+        return "Human controller";
+    }
+}
