cc75ac128c88d346b5a40915c7bf5fb18a0b3727
Bulbash <zverovik@yahoo.com>
2022-05-25 12:16:22 +0300

    created WebConfig and properties files

--- src/main/java/com/alex/config/WebConfig.java
+++ src/main/java/com/alex/config/WebConfig.java
@@ -1,14 +1,63 @@
 package com.alex.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 
 @Configuration
 @EnableWebMvc
+@PropertySources({
+        @PropertySource("classpath:db.properties"),
+        @PropertySource("classpath:hibernate.properties")
+})
+
 @ComponentScan("com.alex")
 public class WebConfig implements WebMvcConfigurer {
 
+    @Resource
+    private Environment environment;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(environment.getRequiredProperty("db.entity.package"));
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setJpaProperties(getHibernateProperties());
+
+        return em;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+
+        return properties;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+
+        ds.setDriverClassName(environment.getRequiredProperty("db.driver"));
+        ds.setUrl(environment.getRequiredProperty("db.url"));
+        ds.setUsername(environment.getRequiredProperty("db.username"));
+        ds.setPassword(environment.getRequiredProperty("db.password"));
+
+        return ds;
+    }
 }
