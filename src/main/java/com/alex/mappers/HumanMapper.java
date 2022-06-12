1d62e6789fcdfbb3afa9725dbcd5839bd5e77bda
Bulbash <zverovik@yahoo.com>
2022-06-12 16:29:54 +0300

    change annotation

--- src/main/java/com/alex/mappers/HumanMapper.java
+++ src/main/java/com/alex/mappers/HumanMapper.java
@@ -3,13 +3,17 @@ package com.alex.mappers;
 import com.alex.dto.HumanDto;
 import com.alex.model.Human;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(uses = PhoneMapper.class)
 public interface HumanMapper {
-
+//    @Mapping(source = "id" , target = "idDto")
+    @Mapping(source = "phones",target = "phoneDtoList")
     HumanDto toDto(Human human);
 
+//    @Mapping(source = "idDto", target = "id")
+    @Mapping(source = "phoneDtoList",target = "phones")
     Human toModel(HumanDto humanDto);
 
     void updateHumanFromDto(HumanDto dto, @MappingTarget Human human);
--- src/main/java/com/alex/mappers/PhoneMapper.java
+++ src/main/java/com/alex/mappers/PhoneMapper.java
@@ -3,13 +3,15 @@ package com.alex.mappers;
 import com.alex.dto.PhoneDto;
 import com.alex.model.Phone;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-@Mapper(uses = HumanMapper.class)
+@Mapper
+//        (uses = HumanMapper.class)
 public interface PhoneMapper {
-
+    @Mapping(source = "human",target = "humanDto")
     PhoneDto toDto(Phone phone);
-
+    @Mapping(source = "humanDto",target = "human")
     Phone toModel(PhoneDto phone);
 
     void updatePhoneFromDto(PhoneDto dto, @MappingTarget Phone phone);
