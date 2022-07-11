7f0c517c5c36329f4b7fb10a9468122fc6f9075b
Bulbash <zverovik@yahoo.com>
2022-07-11 13:25:47 +0300

    make class abstract added block "try/catch" to methods

--- src/main/java/com/alex/repository/AbstractHibernateDao.java
+++ src/main/java/com/alex/repository/AbstractHibernateDao.java
@@ -7,7 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import java.io.Serializable;
 import java.util.List;
 
-public class AbstractHibernateDao<T, PK extends Serializable> implements GenericDao<T, PK> {
+public abstract class AbstractHibernateDao<T, PK extends Serializable> implements GenericDao<T, PK> {
 
     @Autowired
     private final EntityManagerFactory managerFactory;
@@ -25,20 +25,34 @@ public class AbstractHibernateDao<T, PK extends Serializable> implements Generic
     @Override
     public T findOne(PK id) {
         EntityManager entityManager = getEntityManager();
-        entityManager.getTransaction().begin();
-        T entity = entityManager.find(clazz, id);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        return entity;
+        try {
+
+            entityManager.getTransaction().begin();
+            T entity = entityManager.find(clazz, id);
+            entityManager.getTransaction().commit();
+            return entity;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed question findOne", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     public List<T> findAll() {
         EntityManager entityManager = getEntityManager();
-        List<T> list = entityManager
-                .createQuery(String.format("select t from %s t", clazz.getSimpleName()), clazz)
-                .getResultList();
-        entityManager.close();
-        return list;
+        try {
+
+            List<T> list = entityManager
+                    .createQuery(String.format("select t from %s t", clazz.getSimpleName()), clazz)
+                    .getResultList();
+            return list;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed question findAll", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     public T create(final T entity) {
@@ -51,44 +65,70 @@ public class AbstractHibernateDao<T, PK extends Serializable> implements Generic
             return entity;
 
         } catch (Exception e) {
-
             entityManager.getTransaction().rollback();
-            throw new RuntimeException("Create failed",e);
+            throw new RuntimeException("Create failed", e);
         } finally {
-
             entityManager.close();
         }
     }
 
     public T update(final T entity) {
         EntityManager entityManager = getEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.merge(entity);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        return entity;
+        try {
+
+            entityManager.getTransaction().begin();
+            entityManager.merge(entity);
+            entityManager.getTransaction().commit();
+            return entity;
+
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new RuntimeException("Failed update method", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     public void delete(final T entity) {
         EntityManager entityManager = getEntityManager();
-        entityManager.getTransaction().begin();
-        entityManager.remove(entity);
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        try {
+
+            entityManager.getTransaction().begin();
+            entityManager.remove(entity);
+            entityManager.getTransaction().commit();
+
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new RuntimeException("Failed method delete", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     public void deleteById(final PK entityId) {
         EntityManager entityManager = getEntityManager();
-        T entity = entityManager.find(clazz, entityId);
-        entityManager.getTransaction().begin();
-        entityManager.remove(entity);
-        entityManager.getTransaction().commit();
-        entityManager.close();
+        try {
+
+            T entity = entityManager.find(clazz, entityId);
+            entityManager.getTransaction().begin();
+            entityManager.remove(entity);
+            entityManager.getTransaction().commit();
+
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new RuntimeException("Filed method deleteById", e);
+        } finally {
+            entityManager.close();
+        }
     }
 
     public void saveAll(List<T> saveCollection) {
-        for (T dto : saveCollection) {
-            create(dto);
+        if (saveCollection != null) {
+            for (T dto : saveCollection) {
+                create(dto);
+            }
+        }else {
+            throw new RuntimeException("Filed method saveAll, collection is null");
         }
     }
 }
