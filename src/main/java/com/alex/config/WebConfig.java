960b651ca76c2bb811b2128f6565a7b87255b052
Ivan Semenov3 <ivan_semenov3@epam.com>
2022-09-04 19:41:07 +0300

    Added ui swagger v2. http://localhost:8080/swagger/index.html

--- src/main/java/com/alex/config/WebConfig.java
+++ src/main/java/com/alex/config/WebConfig.java
@@ -126,8 +126,9 @@ public class WebConfig implements WebMvcConfigurer {
     }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/resources/**")
+        registry.addResourceHandler("/swagger/**")
+                .addResourceLocations("/WEB-INF/swagger/");
+        registry.addResourceHandler("/resources/**")
                 .addResourceLocations("/WEB-INF/resources/");
     }
 
